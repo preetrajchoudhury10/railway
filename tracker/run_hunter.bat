f38303835c239a08e3e4fd8b6c487474f75f79e9
reetrajchoudhury10 <preet.raj.choudhury10@gmail.com>
2026-06-07 17:31:47 +0530

    Restructure: move railway-app files to repo root for Railway deployment

--- tracker/run_hunter.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python job_hunter.py
-if %errorlevel%==0 (
-    echo Job hunt completed at %date% %time%
-) else (
-    echo Job hunt encountered an error at %date% %time%
-)
